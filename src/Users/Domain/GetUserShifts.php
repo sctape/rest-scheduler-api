f11dccac7cbe30076f8b6bf21cf28a57991c363a
Sam Tape <stape@fisdap.net>
2015-11-18 18:43:04 -0600

    Adding authorization logic to prevent employee's from seeing other employee shifts

--- src/Users/Domain/GetUserShifts.php
+++ src/Users/Domain/GetUserShifts.php
@@ -11,11 +11,13 @@ namespace Scheduler\Users\Domain;
 
 use League\Fractal\Manager;
 use League\Fractal\Resource\Collection;
+use Scheduler\Exception\UserNotAuthorized;
 use Scheduler\Shifts\Repository\ShiftRepository;
 use Scheduler\Shifts\Transformer\ShiftTransformer;
 use Scheduler\Users\Repository\UserRepository;
 use Spark\Adr\DomainInterface;
 use Spark\Adr\PayloadInterface;
+use Spark\Auth\AuthHandler;
 
 class GetUserShifts implements DomainInterface
 {
@@ -56,9 +58,15 @@ class GetUserShifts implements DomainInterface
     /**
      * @param array $input
      * @return PayloadInterface
+     * @throws UserNotAuthorized
      */
     public function __invoke(array $input)
     {
+        //Don't allow employees to view other employee's shifts
+        if ($input['id'] != $input[AuthHandler::TOKEN_ATTRIBUTE]->getMetaData('id')) {
+            throw new UserNotAuthorized;
+        }
+
         $employee = $this->userRepository->getOneById($input['id']);
         $shifts = $this->shiftRepository->getByEmployee($employee);
         $shiftsCollection = new Collection($shifts, new ShiftTransformer);
